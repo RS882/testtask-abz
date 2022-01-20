a912b581ad9146de8fbb546a275bed1241ebfc83
Russen <russen333@gmail.com>
2022-01-20 20:31:56 +0200

    +menu and useMediaQuery

--- src/componrnts/header/Header.jsx
+++ src/componrnts/header/Header.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from './../../assets/img/Logo.svg';
 import BurgerMenu from './burgerMenu/BurgerMenu';
 import MenuContainer from './Menu/Menu';
+import { useMediaQuery } from './../Hook/useMediaQuery';
 
 const Header = (props) => {
 
 	const [activeClass, setActiveClass] = useState('');
 	const onClickBurgerBtn = () => setActiveClass('_active');
 	const onClickNotMenu = () => setActiveClass('_transition');
-
-
-
+	const endTransition = () => {
+		if (activeClass === '_transition') setActiveClass('');
+	};
+	const is1024 = (useMediaQuery('(min-width: 1024px)'));
+	useEffect(() => {
+		if (is1024) setActiveClass('');
+	})
 	return (
 		<header className="header">
-			<div className="header__container container">
+			<div onTransitionEnd={endTransition} className="header__container container">
 				<div className={`header__body ${activeClass}`}>
 					<div className="header__logo logo">
 						<NavLink to={'/login'} className="logo__box">
--- src/componrnts/header/Menu/Menu.jsx
+++ src/componrnts/header/Menu/Menu.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Menu = (props) => {
-
+	const creatMenuItems = (menuItems) => menuItems.map((el, i) => <li className="menu__item" onClick={props.onClickItem} key={i}>
+		<NavLink to={'/login'}>{el}</NavLink></li>);
+	const menuElements = props.isBurgerMenu ?
+		(props.menuItems.map((elem, i) =>
+			<ul className='menu__sublist' key={i}>{creatMenuItems(elem)}</ul>)
+		) : creatMenuItems(props.menuItems)
 	return (
 		<div className="header__menu menu">
 			<ul className="menu__list">
-				{props.menuItems.map((el, i) => <li className="menu__item" onClick={props.onClickItem} key={i}>
-					<NavLink to={'/login'}>{el}</NavLink></li>)}
+				{menuElements}
 			</ul>
 		</div>
 	)
@@ -15,26 +19,23 @@ const Menu = (props) => {
 
 //=========================================================================
 const MenuContainer = (props) => {
-
-
 	const pcMenu = ['About me', 'Relationships', 'Requirements', 'Users', 'Sign Up'];
 	const burgerMenu = [...pcMenu.filter(el => el !== 'Requirements'),
 		'Terms and Conditions', 'How it works', 'Partnership', 'Help',
 		'Level testimonial', 'Contact us', 'Articles', 'Our news', 'Testimonials',
 		'Licenses', 'Privacy Policy'];
-
 	const cutArray = (arr, n = 0) => arr.reduce((res, el, i, ar) => {
 		if (!n) return ar;
 		if (i === 0 || i % n === 0) res.push([]);
 		res[res.length - 1].push(el);
 		return res;
 	}, []);
-
 	const menuItems = props.isBurgerMenu ? cutArray(burgerMenu, 5) : pcMenu;
 
 	const onClickItem = (e) => {
 		console.log(e.target.style.color = `#00BDD3`);
 	}
+
 	return <Menu menuItems={menuItems}
 		onClickItem={onClickItem}
 		isBurgerMenu={props.isBurgerMenu} />
