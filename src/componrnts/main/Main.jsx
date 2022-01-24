2f0a7297ef0b99778910da5b6f78d620c6a5afba
Russen <russen333@gmail.com>
2022-01-24 17:40:15 +0200

    прижат футер

--- src/componrnts/main/Main.jsx
+++ src/componrnts/main/Main.jsx
@@ -4,6 +4,7 @@ import Button from '../button/button';
 import Banner from './Banner/Banner';
 import Article from './Article/Article';
 import Users from './Users/Users';
+import Login from '../LoginPage/LoginPage';
 
 const Main = (props) => {
 
@@ -14,6 +15,7 @@ const Main = (props) => {
 			<Banner />
 			<Article />
 			<Users />
+			<Login />
 
 			{/* <div className='main__wrapper'>
 				<div className='main__bg'>
