8ef85ad39bc8b65b173141466e2383c936c8a4e4
Russen <russen333@gmail.com>
2022-01-27 17:28:23 +0200

    _react final form

--- src/componrnts/LoginPage/LoginPage.jsx
+++ src/componrnts/LoginPage/LoginPage.jsx
@@ -1,17 +1,18 @@
 import Button from "../button/button";
-
+import { Form, Field } from 'react-final-form'
 
 const Login = (props) => {
 
 	const radiosElem = props.positions.map((el, i) => {
 		return <div key={el.id}>
-			<input
+			<Field
+				component={`input`}
 				className="radio__radiobtn"
 				type={`radio`}
 				name="position_id"
 				id={`position_${el.id}`}
 				value={el.id}
-				defaultChecked={!i || false}
+				checked={!i || false}
 			/>
 			<label
 				className="radio__label"
@@ -20,9 +21,8 @@ const Login = (props) => {
 
 	})
 
-	const onSubmit = (e) => {
-		e.preventDefault()
-		console.log(`submit`);
+	const onSubmit = (formData) => {
+		console.log(formData);
 	}
 
 	const formData = {
@@ -39,49 +39,69 @@ const Login = (props) => {
 				<div className="login__wrapper">
 					<h2 className="login__title">{props.title}</h2>
 					<h4 className="login__subtitle">{props.subtitle}</h4>
-					<form className="login__form form">
-						<div>
-							<input
-								className="form__input"
-								type={`text`}
-								name="name"
-								placeholder="Your name" />
-						</div>
-						<div>
-							<input
-								className="form__input"
-								type={`email`}
-								name="email"
-								placeholder="Email" />
-						</div>
-						<div>
-							<input
-								className="form__input"
-								type={`tel`}
-								name="phone"
-								placeholder="Phone" />
-						</div>
-						<div className="form__radiobtns radio">
-							<h5 className="radio__title">Select your position</h5>
-							<div className="radio__radios">
-								{radiosElem}
-							</div>
-						</div>
-						<div>
-							<input
-								className="form__input"
-								type={`file`}
-								name="photo"
-							/>
-						</div>
-						<div className="form__submit">
-							<Button
-								disabled={props.disebledBtn}
-								onClickBtn={onSubmit}
-								btnType={'submit'}
-								text={'Sign up'} />
-						</div>
-					</form>
+
+					<Form
+						onSubmit={(values, form) => {
+							onSubmit(values);
+						}}
+						initialValues={{ ...formData, }}
+						render={({ handleSubmit, form, submitting, pristine, errors }) => {
+
+
+							return (
+								<form onSubmit={handleSubmit} className="login__form form">
+									<div>
+										<Field
+											component={`input`}
+											className="form__input"
+											type={`text`}
+											name="name"
+											placeholder="Your name" />
+									</div>
+									<div>
+										<Field
+											component={`input`}
+											className="form__input"
+											type={`email`}
+											name="email"
+											placeholder="Email" />
+									</div>
+									<div>
+										<Field
+											component={`input`}
+											className="form__input"
+											type={`tel`}
+											name="phone"
+											placeholder="Phone" />
+									</div>
+									<div className="form__radiobtns radio">
+										<h5 className="radio__title">Select your position</h5>
+										<div className="radio__radios">
+											{radiosElem}
+										</div>
+									</div>
+									<div>
+										<Field
+											component={`input`}
+											className="form__input"
+											type={`file`}
+											name="photo"
+										/>
+									</div>
+									<div className="form__submit">
+										<Button
+											disabled={props.disebledBtn}
+											onClickBtn={onSubmit}
+											btnType={'submit'}
+											text={'Sign up'} />
+									</div>
+								</form>
+
+							)
+						}
+						}
+					/>
+
 				</div>
 
 			</div>
