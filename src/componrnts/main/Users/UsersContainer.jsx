849c5c8773f6886d60ac0e52fc18e0edfc498b44
Russen <russen333@gmail.com>
2022-02-07 12:09:54 +0200

    рефакторинг

--- src/componrnts/main/Users/UsersContainer.jsx
+++ src/componrnts/main/Users/UsersContainer.jsx
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 import { addUsers, getUsers } from "../../redux/thunkCreation";
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
+import { changeIsBodyLock } from "../../redux/modalReducer";
 
 
 
@@ -14,15 +15,23 @@ const UsersContainer = (props) => {
 	const dispatch = useDispatch();
 	const users = useSelector(state => state.users.users);
 	const isFetching = useSelector(state => state.users.isFetching)
-
-	const [isShowMore, setIsShowMore] = useState(false);
 	const { is768, is1024 } = useSelector(state => state.mediaQuery.breakPoints);
 
+	const [isShowMore, setIsShowMore] = useState(false);
+
+	const changeIsLock = (is) => dispatch(changeIsBodyLock(is));
+
 	const numShowUsers = is1024 ? 9 : (is768 ? 6 : 3);
+
+	const isShowModal = isFetching && users.users.length > 0
 
 	useEffect(() => {
 		!isShowMore && dispatch(getUsers(numShowUsers))
 	}, [numShowUsers])
+
+	useEffect(() => {
+		changeIsLock(isShowModal)
+	}, [isShowModal])
 
 
 	const title = `Our cheerful users `;
@@ -41,7 +50,7 @@ const UsersContainer = (props) => {
 		subtitle={subtitle}
 		disebledBtn={isFetching}
 		hiddenBtn={users.nextPage === null || +users.totalUsers == users.length}
-		showModal={isFetching && users.users.length > 0}
+		showModal={isShowModal}
 	/>
 }
 
