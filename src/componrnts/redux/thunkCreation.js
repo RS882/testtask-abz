e8ac8dc5aa5b3caefd63687004946c600a17e430
Russen <russen333@gmail.com>
2022-02-07 12:33:20 +0200

    thunk рефакторинг

--- src/componrnts/redux/thunkCreation.js
+++ src/componrnts/redux/thunkCreation.js
@@ -1,11 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { userAPI } from './../../api/api';
 
-
-export const getUsers = createAsyncThunk(
-	'users/getUsers',
-	async (pageSize, thunkAPI) => {
-		const res = await userAPI.getUsers(pageSize)
+const usersThunk = (name, funcAPI) => createAsyncThunk(
+	name,
+	async (arg, thunkAPI) => {
+		const res = await funcAPI(arg)
 			.then(response => response.data);
 		if (res.success) {
 			return res;
@@ -13,20 +12,7 @@ export const getUsers = createAsyncThunk(
 			return thunkAPI.rejectWithValue(res);
 		}
 	}
-);
+)
 
-export const addUsers = createAsyncThunk(
-	'users/addUsers',
-	async (url, thunkAPI) => {
-		const res = await userAPI.getNextPage(url)
-			.then(response => {
-				//console.log(response);
-				return response.data
-			});
-		if (res.success) {
-			return res;
-		} else {
-			return thunkAPI.rejectWithValue(res);
-		}
-	}
-);
+export const getUsers = usersThunk('users/getUsers', userAPI.getUsers);
+export const addUsers = usersThunk('users/addUsers', userAPI.getNextPage);
