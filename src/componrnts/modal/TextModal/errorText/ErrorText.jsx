3feed49a0ff2c75fddefd9483bb662a726fd471a
Russen <russen333@gmail.com>
2022-02-09 12:49:43 +0200

    рефакторинг error

--- src/componrnts/modal/TextModal/errorText/ErrorText.jsx
+++ src/componrnts/modal/TextModal/errorText/ErrorText.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 
 const ErrorText = (props) => {
+
 	const errorMessage = useSelector(state => state.users.errorMessage);
 	const addErrorText = (errorMessage) => errorMessage !== undefined && errorMessage.map((el, i) => <div key={i}>{el}</div>);
-
-
+	const errorElem = [];
+	for (const key in errorMessage.fails) errorElem.push(addErrorText(errorMessage.fails[key]));
 
 	return (
 		<>
@@ -12,10 +13,8 @@ const ErrorText = (props) => {
 				<div>
 					{typeof (errorMessage) == "string" && <div > {errorMessage}</div>}
 					{errorMessage.message !== undefined && <div>	{errorMessage.message}</div>}
-					{errorMessage.fails !== undefined && <div>
-						{addErrorText(errorMessage.fails.count)}
-						{addErrorText(errorMessage.fails.page)}
-					</div>}
+					{errorMessage.fails !== undefined && <div>{errorElem}</div>
+					}
 				</div >
 			)}
 
