cfb73657ad8495d7e6ecfac17b477ca0ffb4382f
Russen <russen333@gmail.com>
2022-02-13 15:50:48 +0200

    add comment

--- src/componrnts/utilits/functions.js
+++ src/componrnts/utilits/functions.js
@@ -1,3 +1,6 @@
+// резрезаем массив на равные части. 
+//результат -массив массивов с n- количеством элементов в каждом
+//(в посленем - кол. может быть меньше n)
 export const cutArray = (arr, n = 0) => arr.reduce((res, el, i, ar) => {
 	if (!n) return ar;
 	if (i === 0 || i % n === 0) res.push([]);
@@ -6,39 +9,32 @@ export const cutArray = (arr, n = 0) => arr.reduce((res, el, i, ar) => {
 }, []);
 
 //-----------------------------------------------------------
-
+//обрезаем строку на величину num, довавляя в конце строки endOfStr
 export const cutString = (str, num = 10, endOfStr = '...') => {
 	if (str.length > num) return str.substr(0, num) + endOfStr;
 	return str;
 };
-//--------------------------------------------------------------
-export const cutTextWithDot = (text, is768, is1024, cutFromDot = true) => {
-	const dotPos = text.indexOf('.') + 1;
-	let text768;
-	if (cutFromDot) {
-		text768 = (dotPos && dotPos < 117) ? cutString(text, dotPos, '') :
-			cutString(text, 120);
-	} else {
-		text768 = cutString(text, 315);
-	}
-	const textMod = !is768 ? text768 : cutString(text, 365);
-	return !is1024 ? textMod : cutString(text, 860);
-};
+
 //---------------------------------------------------------------
+// определяем размер изображения в файле file и передаем его в функцию setFunc
 export const readImageSize = (file, setFunc) => {
 	const reader = new FileReader();
-	reader.readAsDataURL(file);
-	reader.onload = () => {
-		const image = new Image();
-		image.src = reader.result;
-		image.onload = () => {
-			setFunc({ width: image.width, height: image.height, });
+	if (file) {
+		reader.readAsDataURL(file);//читаем содержимое file
+		reader.onload = () => {
+			const image = new Image();//создаем объект img
+			image.src = reader.result;
+			image.onload = () => {
+				setFunc({ width: image.width, height: image.height, });
+			};
 		};
-	};
+	} else { setFunc({}); }
 }
 //------------------------------------------------------------
+//преобразуем шаблон пока тел номера из +3801111111 в +38 (011) 111 11 11
 export const showPhoneNum = phoneNum => {
 	const res = phoneNum.split('');
+	// изменем массив arr : на позиции указанные в массиве arrModPos вставляем строку modElem
 	const arrMod = (arr, arrModPos, modElem = ' ') => arrModPos.forEach(e => arr.splice(e, 0, modElem));
 	arrMod(res, [3, 7, 11, 14]);
 	arrMod(res, [4], '(');
